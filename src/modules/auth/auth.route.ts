f5e36e1c84790becaa4c61fd07affa58cc3a2ebd
Tanjim Siddiki Siyam <siam88536@gmail.com>
2025-07-18 03:37:58 +0600

    updated user validation for sign up

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -1,9 +1,9 @@
 
 import express from 'express';
 import validateRequest from '../../middleware/validateRequest';
-import { userValidationSchema } from '../users/user.validation';
 import { loginValidationSchema } from './auth.validation';
 import { authControllers } from './auth.controller';
+import { userValidationSchema } from '../users/user.validation';
 const router = express.Router();
 
 router.post('/signup', validateRequest(userValidationSchema) , authControllers.registerUser );
--- src/modules/users/user.validation.ts
+++ src/modules/users/user.validation.ts
@@ -1,4 +1,3 @@
-
 import { z } from "zod";
 
 export const userValidationSchema = z.object({
